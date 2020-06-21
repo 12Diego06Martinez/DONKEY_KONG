25ffd1c69f821efbcd234a722f5c94820eb7f3f5
Diego_Martinez <diego.martinezn@alumnos.upm.es>
2020-06-21 18:30:43 +0200

    Creada la escalera

--- DONKEY_KONG/DONKEY_KONG/src/Mundo.h
+++ DONKEY_KONG/DONKEY_KONG/src/Mundo.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Jugador.h"
 #include "Plataforma.h"
+#include "Escalera.h"
 
 class Mundo
 {
@@ -9,8 +10,14 @@ private:
 	float y_ojo;
 	float z_ojo;
 	Jugador player;
-	//Plataforma plataforma1;
 	Plataforma* plataforma1 = new Plataforma(false);
+	Plataforma* plataforma2 = new Plataforma(false);
+	Plataforma* plataforma3 = new Plataforma(false);
+	Plataforma* plataforma4 = new Plataforma(false);
+	Escalera* escalera1 = new Escalera(false);
+	Escalera* escalera2 = new Escalera(false);
+	Escalera* escalera3 = new Escalera(false);
+
 public:
 	Mundo() {}
 	virtual ~Mundo();
